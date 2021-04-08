c772c840c9907cf72fdd610d32d407d37a423b32
bugrami456asd <30727573+bugrami456asd@users.noreply.github.com>
2021-04-08 00:11:40 +0000

    Add files via upload

--- src/network/server.js
+++ src/network/server.js
@@ -1,7 +1,7 @@
-const uWS = require("uwebsockets.js");
+const uWS = require("uWebSockets.js");
 const Socket = require("./socket");
 
-const ChatChannel = require("./chatChannel");
+const ChatChannel = require("./chatchannel");
 
 /** @param {ArrayBuffer} buffer */
 const bufferToString = buffer => new Uint8Array(buffer).map(ch => String.fromCharCode(ch)).join("");
@@ -23,30 +23,27 @@ module.exports = class SocketServer {
                 maxPayloadLength: 512,
                 compression: uWS.DEDICATED_COMPRESSOR_4KB,
                 upgrade: (res, req, context) => {
-          //          console.log('Connection received from: "' + req.getUrl() + '" ip: ' + new Uint8Array(res.getRemoteAddress()).join("."));
-                  
-                  console.log('Connection');
-                  
-                  
-                    res.upgrade({ url: req.getUrl() },req.getHeader('sec-websocket-key'),req.getHeader('sec-websocket-protocol'),req.getHeader('sec-websocket-extensions'),context);
+                    console.log('Connection received from: "' + req.getUrl() + '" ip: ' + new Uint8Array(res.getRemoteAddress()).join("."));
+                    res.upgrade({ url: req.getUrl() },
+                        req.getHeader('sec-websocket-key'),
+                        req.getHeader('sec-websocket-protocol'),
+                        req.getHeader('sec-websocket-extensions'),
+                        context);
                 },
                 open: ws => {
                     ws.sock = new Socket(this.game, ws);
                     this.game.addHandle(ws.sock);
-                  console.log('d')
                 },
                 message: (ws, message, isBinary) => {
                     if (!isBinary) ws.end(1003);
                     ws.sock.onMessage(new DataView(message));
                 },
                 close: (ws, code, message) => {
-                //    console.log(`Disconnected: (handle#${ws.sock.controller.id}) code: ${code}, message: ${bufferToString(message)}`);
-                  console.log('Disconnected')
-                 //   this.game.removeHandle(ws.sock);
-                  this.game.removePlayerFull(ws.sock, ws);
+                    console.log(`Disconnected: (handle#${ws.sock.controller.id}) code: ${code}, message: ${bufferToString(message)}`);
+                    this.game.removeHandle(ws.sock);
                 }
             }).listen("0.0.0.0", 5000, sock => {
-                this.listening = true;
+                this.listening = false;
                 this.sock = sock;
                 this.game.chatChannel = new ChatChannel(this.game);
                 console.log(`Server opened`);
